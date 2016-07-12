18662993aa101e9e7012f7d9f76a91931ebf669e
Softsprocket <greg@softsprocket.com>
2016-07-12 10:07:25 -0700

    testing of vector

--- js/tests.js
+++ js/tests.js
@@ -6,8 +6,8 @@ requirejs.config({
     	}
 });
 
-requirejs (['app/math/vec3', 'app/math/vec4'],
-function (Vec3, Vec4) {
+requirejs (['app/math/vec3', 'app/math/vec4', 'app/math/vec2'],
+function (Vec3, Vec4, Vec2) {
 	var contentEl = document.getElementById ('test-content-id');
 	
 	var p = Vec3 (2, 2, 1);
@@ -28,6 +28,11 @@ function (Vec3, Vec4) {
 	var dotProduct4 = Vec4.DotProduct (p4, q4);
 	var proj4 = Vec4.Projection (q4, p4);
 	
+	var p2 = Vec2 (2, 2);
+	var q2 = Vec2 (1, -2);
+	var dotProduct2 = Vec2.DotProduct (p2, q2);
+	var proj2 = Vec2.Projection (q2, p2);
+
 	var html = '<pre>';
 	html += 'Let P = &lt;2,2,1&gt; and Q = &lt;1,-2,0&gt;.\n';
 	html += 'P&#149;Q = ' + dotProduct + '.\n';
@@ -41,13 +46,20 @@ function (Vec3, Vec4) {
 	html += 'set [&lt;' + eortho[0][0] + ', ' + eortho[0][1] + ', ' + eortho[0][2] + '&gt;, &lt;' 
 		+ eortho[1][0] + ', ' + eortho[1][1] + ', ' + eortho[1][2] + '&gt;, &lt;' 
 		+ eortho[2][0] + ', ' + eortho[2][1] + ', ' + eortho[2][2] + '&gt;]\n';
+	
 	html += '\n\n';
-
 
 	html += 'Let P = &lt;2,2,1,1&gt; and Q = &lt;1,-2,0,1&gt;.\n';
 	html += 'P&#149;Q = ' + dotProduct4 + '.\n';
 	html += 'proj<sub>p</sub>Q = &lt;' + proj4[0] + ', ' + proj4[1] + ', ' + proj4[2] + ', ' + proj4[3] + '&gt;.\n';	
 	html += '\n';	
+	html += '\n\n';
+
+	html += 'Let P = &lt;2,2&gt; and Q = &lt;1,-2&gt;.\n';
+	html += 'P&#149;Q = ' + dotProduct2 + '.\n';
+	html += 'proj<sub>p</sub>Q = &lt;' + proj2[0] + ', ' + proj2[1] + '&gt;.\n';	
+	html += '\n';
+
 	html += '</pre>';
 
 	contentEl.innerHTML = html; 
